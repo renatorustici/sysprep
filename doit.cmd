8eb58f4391857167836768f35d26ae6cf6fbbd4c
Renato Rustici <66761865+renatorustici@users.noreply.github.com>
2022-04-05 17:17:50 -0300

    Update doit.cmd

--- doit.cmd
+++ doit.cmd
@@ -1,3 +1,4 @@
 C:\Windows\System32\Sysprep\sysprep.exe /oobe /generalize /quit /unattend:C:\windows\sysprep-unattended.xml
 net user Administrator /active:yes /logonpasswordchg:NO
 net user Administrator YourP4sswdG@esHer3
+shutdown /s /t 0
