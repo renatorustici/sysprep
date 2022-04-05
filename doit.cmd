ff85a6c1ed7b77053f68cda5167151c7888b0fcd
Renato Rustici <66761865+renatorustici@users.noreply.github.com>
2022-04-05 17:16:38 -0300

    Create doit.cmd

---
+++ doit.cmd
@@ -0,0 +1,3 @@
+C:\Windows\System32\Sysprep\sysprep.exe /oobe /generalize /quit /unattend:C:\windows\sysprep-unattended.xml
+net user Administrator /active:yes /logonpasswordchg:NO
+net user Administrator YourP4sswdG@esHer3
